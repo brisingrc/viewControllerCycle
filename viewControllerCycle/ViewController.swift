c85f676c2864a3c2a8a1fb26c8b7b3a1eaf7ac62
Aidar <brisingr@inbox.ru>
2019-04-28 23:44:53 +0600

    First commit

--- viewControllerCycle/ViewController.swift
+++ viewControllerCycle/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var blueTextView: UITextView!
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        Addition.shared.printMessage(textView: blueTextView, viewController: self)
     }
 
 
 }
 
+extension ViewController {
+    
+    
+    
+}
